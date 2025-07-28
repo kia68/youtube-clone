dd3f647e4d4bc35c3214abe356731dd8326d1595
kia68 <kiavash.emami@hhu.de>
2025-07-28 19:55:27 +0200

    backend and frontend maven Module,angular and MongoDB started

--- backend/src/main/java/com/example/kiatech/youtube_clone/YoutubeCloneApplication.java
+++ backend/src/main/java/com/example/kiatech/youtube_clone/YoutubeCloneApplication.java
@@ -2,12 +2,20 @@ package com.example.kiatech.youtube_clone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class YoutubeCloneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YoutubeCloneApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return "Welcome to the YouTube Clone Application!";
 	}
 
 }
